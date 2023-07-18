d8071650a53b91c87b13d303a45c1ee6fa83420e
Pedro Ferreira Monteiro Filho <20211170150004@ifrn.local>
2023-07-18 08:13:12 -0300

    google drive

---
+++ My project (1)/Assets/Scripts/Flecha.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flecha : MonoBehaviour
+{
+
+    private Rigidbody2D rig;
+    public float speed;
+
+    public int damage;
+
+    public bool isRight;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 2f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isRight)
+        {
+            rig.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            rig.velocity = Vector2.left * speed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.GetComponent<Monstro>().Damage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
--- My project (1)/Assets/Scripts/Player.cs
+++ My project (1)/Assets/Scripts/Player.cs
@@ -6,41 +6,76 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    public int health = 3;
     public float speed;
     public float jumpForce;
-    private bool isJumping;
 
+    public GameObject bow;
+    public Transform firePoint;
+    
+    private bool isJumping;
+    private bool doubleJump;
+    private bool isFire;
+    
     private Rigidbody2D rig;
     private Animator anim;
+
+    private float movement;
     
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        
+        GameController.instance.UpdateLives(health);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        BowFire();
         Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
     }
 
     void Move()
     {
-        float movement = Input.GetAxis("Horizontal");
+        // se nao pressionar nada valor igual é 0. Pressionar direita, valor maximo 1. Esquerda valor maximo -1
+        movement = Input.GetAxis("Horizontal");
 
+        //adiciona velocidade ao corpo do personagem no eixo x e y
         rig.velocity = new Vector2(movement * speed, rig.velocity.y);
 
+        // andando pra direita
         if (movement > 0)
         {
+            if (!isJumping)
+            {
+                anim.SetInteger("transition", 1);
+            }
+            
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
 
+        //andando pra esquerda
         if (movement < 0)
         {
+            if (!isJumping)
+            {
+                anim.SetInteger("transition", 1);
+            }
+            
             transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (movement == 0 && !isJumping && !isFire)
+        {
+            anim.SetInteger("transition", 0);
         }
     }
 
@@ -50,9 +85,59 @@ public class Player : MonoBehaviour
         {
             if (!isJumping)
             {
+                anim.SetInteger("transition", 2);
                 rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
                 isJumping = true;
             }
+            else
+            {
+                if (doubleJump)
+                {
+                    anim.SetInteger("transition", 2);
+                   rig.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
+                   doubleJump = false;
+                }
+            }
+        }
+    }
+
+    void BowFire()
+    {
+        StartCoroutine("Fire");
+    }
+
+    IEnumerator Fire()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            isFire = true;
+            anim.SetInteger("transition", 3);
+            GameObject Bow =Instantiate(bow, firePoint.position, firePoint.rotation);
+
+            if (transform.rotation.y == 0)
+            {
+                Bow.GetComponent<Flecha>().isRight = true;
+            }
+            if (transform.rotation.y == 180)
+            {
+                Bow.GetComponent<Flecha>().isRight = false;
+            }
+            
+            yield return new WaitForSeconds(0.18f);
+            isFire = false;
+            anim.SetInteger("transition", 0);
+        }
+    }
+
+    public void Damage(int dmg)
+    {
+        health -= dmg;
+        GameController.instance.UpdateLives(health);
+
+        if (health <= 0)
+        {
+            //chamar game over
         }
     }
 
