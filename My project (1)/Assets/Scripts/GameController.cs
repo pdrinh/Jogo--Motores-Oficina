e949effdbb647179311a766623d1e9492323ef02
Pedro Ferreira Monteiro Filho <20211170150004@ifrn.local>
2023-07-21 08:42:38 -0300

    save das moedas

--- My project (1)/Assets/Scripts/GameController.cs
+++ My project (1)/Assets/Scripts/GameController.cs
@@ -10,12 +10,19 @@ public class GameController : MonoBehaviour
     public int score;
     public Text scoreText;
 
+    public int totalScore;
+
     public static GameController instance;
     
     // Awake é inicializado antes de todos os métodos start() do seu projeto
     void Awake()
     {
         instance = this;
+    }
+
+    void Start()
+    {
+        totalScore = PlayerPrefs.GetInt("score");
     }
 
     // Update is called once per frame
@@ -28,6 +35,8 @@ public class GameController : MonoBehaviour
     {
         score += value;
         scoreText.text = score.ToString();
+        
+        PlayerPrefs.SetInt("score", score + totalScore);
     }
 
     public void UpdateLives(int value)
