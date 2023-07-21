4b66822e9ef8853eb8da98d541e83bceba70084a
Pedro Ferreira Monteiro Filho <20211170150004@ifrn.local>
2023-07-21 08:27:40 -0300

    moeda

--- My project (1)/Assets/Scripts/GameController.cs
+++ My project (1)/Assets/Scripts/GameController.cs
@@ -7,6 +7,9 @@ public class GameController : MonoBehaviour
 {
     public Text healthText;
 
+    public int score;
+    public Text scoreText;
+
     public static GameController instance;
     
     // Awake é inicializado antes de todos os métodos start() do seu projeto
@@ -19,6 +22,12 @@ public class GameController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void UpdateScore(int value)
+    {
+        score += value;
+        scoreText.text = score.ToString();
     }
 
     public void UpdateLives(int value)
