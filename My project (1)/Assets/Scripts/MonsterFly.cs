3eb77d1078dfe060ad10ebf0a6b9f7599ee4583c
Pedro Ferreira Monteiro Filho <20211170150004@ifrn.local>
2023-07-31 17:03:00 -0300

    Julia - Modificação no Mapa do jogo
    extandendo o mapa

---
+++ My project (1)/Assets/Scripts/MonsterFly.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MonsterFly : MonoBehaviour
+{
+    
+    public float speed;
+    public float walkTime;
+    public bool walkRight = true;
+
+    public int health;
+    public int damage = 1;
+
+    private float timer;
+
+    private Animator anim;
+    private Rigidbody2D rig;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= walkTime)
+        {
+            walkRight = !walkRight;
+            timer = 0f;
+        }
+
+        if (walkRight)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            rig.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            rig.velocity = Vector2.left * speed;
+        }
+    }
+    
+    public void Damage(int dmg)
+    {
+        health -= dmg;
+        anim.SetTrigger("hit");
+
+        if (health <= 0)
+        {
+            //destroi o inimigo
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Player>().Damage(damage);
+        }
+    }
+}
